a26b856e9d216eca3c1fce64c8c1059ba0302176
Rimeset <117002703+Rimeset@users.noreply.github.com>
2023-12-31 09:54:25 +0100

    Add files via upload

---
+++ PRO-C115_TP/file_system_move_File.py
@@ -0,0 +1,6 @@
+import os
+
+src_path= 'E:/My_stuff/Documents/My_folder/Coding/Python/Projects/PRO-C115_TP/main.txt'
+dst_path='E:/My_stuff/Documents/My_folder/Coding/Python/Projects/PRO-C115_TP/newfile.txt'
+
+os.rename(src_path,dst_path)
